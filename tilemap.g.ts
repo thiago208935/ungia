05037911dacac83a0479d77f6872600edf5e0547
thiago208935 <118915050+thiago208935@users.noreply.github.com>
2022-11-23 17:31:33 +0100

    Archivos iniciales del proyecto de MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "nivel1":
+            case "nivel1":return tiles.createTilemap(hex`1000100001010101010101010101010201010101010101010101010201020202020202010101010102010202020202020202010101010101010202010202020202020101010102020102020201020202020101010101020102020202020202010201020201010201020102010202020201020202010102020102020201020202020201020101020102020202020102010202010201010202020102020102010202010102010102020202020102010102020101020101010202020202020101020101010201010202020202020101020101010202010102020202020202020102020201010101020202020202010101020101010101010202020101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
